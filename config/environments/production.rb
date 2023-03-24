d9cde7c522a1b748a59953262b3acf58531396e4
Chandresh Gupta <43450858+guptachandresh@users.noreply.github.com>
2023-03-24 19:11:49 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,6 +99,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+	
+  # Adding hostname functionality
+  config.hosts << /[a-z0-9]+\.ondigitalocean\.app/
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
